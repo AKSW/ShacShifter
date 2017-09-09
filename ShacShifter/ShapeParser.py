79b29302f0a495a5d5a01c0563461b6c8d71f00e
shinobu <niehoff.fabian@web.de>
2017-09-09 21:43:37 +0200

    add variables to propertyshape class and add function to parse the path

--- ShacShifter/ShapeParser.py
+++ ShacShifter/ShapeParser.py
@@ -27,6 +27,8 @@ class ShapeParser:
             print(shape.nodeKind)
             for message in shape.message:
                 print(message.lang + ':' + message)
+            for property in shape.properties:
+                print(property.path)
             print('-------------------------------------------------------------')
 
     def getNodeShapeUris(self, g):
@@ -107,7 +109,61 @@ class ShapeParser:
         return nodeShape
 
     def parsePropertyShape(self, g, shapeUri):
+        sh = rdflib.Namespace('http://www.w3.org/ns/shacl#')
         propertyShape = PropertyShape()
+        if shapeUri != rdflib.term.BNode(shapeUri):
+            propertyShape.uri = shapeUri
+        pathStart = g.value(subject=shapeUri, predicate=sh.path)
+        propertyShape.path = self.getPropertyPath(g, pathStart)
+        
         return propertyShape
+
+    def getPropertyPath(self, g, pathUri):
+        rdf = rdflib.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+        sh = rdflib.Namespace('http://www.w3.org/ns/shacl#')
+        #not enforcing blank nodes here, but stripping the link nodes from the data structure
+        if not (g.value(subject=pathUri, predicate=rdf.first) is None):
+            newPathUri = g.value(subject=pathUri, predicate=rdf.first)
+            rdfList = []
+            rdfList.append(self.getPropertyPath(g, newPathUri))
+            if not (g.value(subject=pathUri, predicate=rdf.rest) == rdf.nil):
+                newPathUri = g.value(subject=pathUri, predicate=rdf.rest)
+                rest = self.getPropertyPath(g, newPathUri)
+                if (isinstance(rest, list)):
+                    rdfList += rest
+                else:
+                    rdfList.append(rest)
+            return rdfList
+
+        if not (g.value(subject=pathUri, predicate=sh.alternativePath) is None):
+            newPathUri = g.value(subject=pathUri, predicate=sh.alternativePath)
+            rdfDict = {sh.alternativePath: self.getPropertyPath(g,newPathUri)}
+            return rdfDict
+
+        if not (g.value(subject=pathUri, predicate=sh.inversePath) is None):
+            newPathUri = g.value(subject=pathUri, predicate=sh.inversePath)
+            rdfDict = {sh.inversePath: self.getPropertyPath(g,newPathUri)}
+            return rdfDict
+
+        if not (g.value(subject=pathUri, predicate=sh.zeroOrMorePath) is None):
+            newPathUri = g.value(subject=pathUri, predicate=sh.zeroOrMorePath)
+            rdfDict = {sh.zeroOrMorePath: self.getPropertyPath(g,newPathUri)}
+            return rdfDict
+
+        if not (g.value(subject=pathUri, predicate=sh.oneOrMorePath) is None):
+            newPathUri = g.value(subject=pathUri, predicate=sh.oneOrMorePath)
+            rdfDict = {sh.oneOrMorePath: self.getPropertyPath(g,newPathUri)}
+            return rdfDict
+
+        if not (g.value(subject=pathUri, predicate=sh.zeroOrOnePath) is None):
+            newPathUri = g.value(subject=pathUri, predicate=sh.zeroOrOnePath)
+            rdfDict = {sh.zeroOrOnePath: self.getPropertyPath(g,newPathUri)}
+            return rdfDict
+
+        #last Object in this Pathpart, check if its a Uri and return it
+        if pathUri == rdflib.term.URIRef(pathUri):
+            return pathUri
+        else:
+            raise Exception('Object of sh:path is no URI')
 
 # class Shape:
--- ShacShifter/modules/PropertyShape.py
+++ ShacShifter/modules/PropertyShape.py
@@ -1,5 +1,30 @@
 class PropertyShape:
-    def __init__(self):
-        self.uri = '' #stays empty if its a blank node/in the nodeshape
-        self.path = ''
     #everything that has sh:path as predicate
+    def __init__(self):
+        self.uri = '' #stays empty if its a blank node
+        self.path = ''
+        self.shClasses = []
+        self.dataType = ''
+        self.minCount = ''
+        self.maxCount = ''
+        self.minExclusive = ''
+        self.minInclusive = ''
+        self.maxExclusive = ''
+        self.MaxInclusive = ''
+        self.minLength = ''
+        self.maxLength = ''
+        self.pattern = ''
+        self.flags = ''
+        self.languageIn = []
+        self.uniqueLang = False
+        self.equals = []
+        self.disjoint = []
+        self.lessThan = []
+        self.lessThanOrEquals = []
+        self.nodes = []
+        #self.qualifiedValueShape = [] #Not exactly sure how multiple qVS are possible with personal min/max counts in the same property
+        #self.qualifiedValueShapesDisjoint = False
+        #self.qualifiedMinCount = ''
+        #self.qualifiedMaxCount = ''
+        self.hasValue = []
+        self.shIn = []

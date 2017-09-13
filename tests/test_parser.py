75fa12130104c7f5b2037c743f293d6b4a8f4bad
Natanael Arndt <arndtn@gmail.com>
2017-09-13 14:50:38 +0200

    Add test layout

---
+++ tests/test_parser.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import unittest
+
+
+class TestParser(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testNothing(self):
+        pass

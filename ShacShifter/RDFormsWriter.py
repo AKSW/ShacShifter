1f0a394c10b76f4c76011df2202bb22de6036a89
Shinobu <niehoff.fabian@web.de>
2017-12-12 13:45:49 +0100

    added choices type

--- ShacShifter/RDFormsWriter.py
+++ ShacShifter/RDFormsWriter.py
@@ -50,9 +50,58 @@ class RDFormsWriter:
         else:
             if propertyShape.isSet['shIn']:
                 # choice type
-                pass
+                choiceItem = '"id":"' + propertyShape.path + '",\n"type":"choice",\n'
+                self.logger.warning('Labels for choice items could be needed as well (message?)')
+                if propertyShape.isSet['message']:
+                    choiceItem += '"label":{'
+                    for lang, message in propertyShape.message.items():
+                        choiceItem += '"' + lang + '":"' + message + '", '
+                    choiceItem = choiceItem[0:-3]
+                    choiceItem += '},\n'
+                self.logger.warning('description exists as well again...')
+                choiceItem += '"property": "' + propertyShape.path + '",\n'
+                if propertyShape.isSet['minCount']:
+                    choiceItem += '"cardinality":{"min": ' + propertyShape.minCount + ','
+                else:
+                    choiceItem += '"cardinality":{"min": 1,'
+                if propertyShape.isSet['maxCount']:
+                    choiceItem += ' "max": ' + propertyShape.maxCount + '}\n'
+                else:
+                    choiceItem += ' "max": 1}\n'
+                choiceItem += 'choices: [{\n' + '"value: "' + propertyShape.path + '",\n'
+                self.logger.warning('choices and single choice items have labels and descriptions again')
+                choiceItem += '"children":[\n'
+                for index in range(0, len(propertyShape.shIn))
+                    choiceItem += '{"_reference: "' + propertyShape.shIn[index] + '"}'
+                    choices += '{"value": "' + propertyShape.shIn[index] + '"},\n'
+                    choices += '{"label": "' + propertyShape.shIn[index] + '"}\n}'
+                    if index != len(propertyShape.shIn -1):
+                        choiceItem += ',\n'
+                        choices += ','
+
+                choiceItem += ']\n},'
+                choiceItem += choices + '\n]'
             else:
                 # text type
                 pass
 
 # x = RDFormsWriter()
+
+choices: [{
+        "top":true,
+        "value": "http://example.com/instanceTop",
+        "selectable": false,
+        "label": {"sv": "Toppen", "en":"Ze top!"},
+        "children":[
+            {"_reference": "http://example.com/instance1"},
+            {"_reference": "http://example.com/instance2"}
+        ]
+    },{
+        "value": "http://example.com/instance1",
+        "label": {"sv": "Matematik", "en":"Mathematics"},
+        "description": {"sv": "Matematik är ett coolt ämne", "en":"Mathematics is a cool subject"}
+    },{
+        "value": "http://example.com/instance2",
+        "label": {"sv": "Kemi", "en":"Chemistry"}
+    }
+]

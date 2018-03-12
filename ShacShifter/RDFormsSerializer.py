95264212e356e5a0fc402cabdb321f4f36a33b7b
Norman Radtke <norman.radtke@gmail.com>
2018-03-12 15:50:34 +0100

    Add methods to get a better overview.

--- ShacShifter/RDFormsSerializer.py
+++ ShacShifter/RDFormsSerializer.py
@@ -21,10 +21,14 @@ class RDFormsSerializer:
             self.logger.error('Can''t write to file {}'.format(outputfile))
             self.logger.error('Content will be printed to sys.')
 
-        self.content.append('{')
         nodeShapes = shapes[0]
+        self.propertyShapes = shapes[1]
+
+        self.content.append('{\n')
+
         for nodeShape in nodeShapes:
             self.nodeShapeEvaluation(nodeShapes[nodeShape])
+
         self.content.append('}')
 
     def write(self):
@@ -44,48 +48,105 @@ class RDFormsSerializer:
         args:   nodeShape a nodeShape object
         """
         def addNodeLabel():
-            # TODO generic label via labels of targets?
-            if nodeShape.isSet['message']:
-                self.content.append('"label":{')
-                labels = ''
-                for lang, message in nodeShape.message.items():
-                    labels += ('"' + lang + '":"' + message + '", ')
-                self.content.append(labels[0:-3] + '},\n')
+            if nodeShape.isSet['targetClass']:
+                label = '"en":"Create new ' + ', '.join(nodeShape.targetClass) + '"'
+            if nodeShape.isSet['targetNode']:
+                label = '"en":"Edit ' + ', '.join(nodeShape.targetNode) + '"'
+            if nodeShape.isSet['targetObjectsOf']:
+                label = '"en":"Edit ' + ', '.join(nodeShape.targetObjectsOf) + '"'
+            if nodeShape.isSet['targetSubjectsOf']:
+                label = '"en":"Edit ' + ', '.join(nodeShape.targetSubjectsOf) + '"'
+            return '  "label":{' + label + '},\n'
 
         def addNodeDescription():
-            self.logger.debug("""Description just like label has no correct
-                sh:equvivalent on core shacl -> messages could be used""")
+            descriptions = ''
+            if nodeShape.isSet['message']:
+                for lang, message in nodeShape.message.items():
+                    descriptions += ('"' + lang + '":"' + message + '", ')
+            return '  "description":{' + descriptions + '},\n'
 
         def addNodeRoot():
-            self.content.append('"root":"' + nodeShape.uri + '",')
+            return '  "root":"' + nodeShape.uri + '",\n'
+
+        def addTemplates():
+            """Check Propertey Shapes to fill the templates."""
+            templates = ['  "templates":[\n']
+            for id, propertyShape in self.propertyShapes.items():
+                templates.append(self.getTemplate(propertyShape))
+            templates.append('\n    ]\n')
+            return ''.join(templates)
 
         self.content.append(addNodeLabel())
         self.content.append(addNodeDescription())
         self.content.append(addNodeRoot())
-
-        self.logger.debug("""Unsure how to handle the sh:targetClass, targetSubjectOf and
-            targetObjectOf properties, should they be constraints for all templates?""")
-
-        self.logger.debug('sh:targetNode has no use for forms')
-        self.logger.debug("""sh:closed, sh:ignoredProperties, sh:nodeKind and sh:severity
-            arent useful for forms either""")
-
+        # self.content.append(addTemplates())
         if len(nodeShape.properties) > 0:
-            self.content.append('"templates":[\n')
-            for property in nodeShape.properties:
-                propertyString = ''
-                propertyString += ('{\n' + self.propertyShapeEvaluation(property) + '\n},')
-            self.content.append(']\n')
+            self.content.append(addTemplates())
 
-        self.content.append(propertyString[0:-3] + ']\n}')
-
-    def propertyShapeEvaluation(self, propertyShape):
-        """Evaluate a propertyShape and return HTML.
+    def getTemplate(self, propertyShape):
+        """Evaluate a propertyShape to serialize a template section.
 
         args:   propertyShape a propertyShape object
-        return: choiceItem string
+        return: template string
         """
-        choiceItem = ''
+        def getId():
+            return '\t\t"id":"' + propertyShape.path + '",\n'
+
+        def getProperty():
+            return '\t\t"property":"' + propertyShape.path + '",\n'
+
+        def getType():
+            if propertyShape.isSet['shIn']:
+                return '\t\t"type":"choice",\n' + getChoices()
+            else:
+                return '\t\t"type":"string",\n'
+
+        def getLabel():
+            label = propertyShape.name \
+                if propertyShape.isSet['name'] else propertyShape.path.rsplit('/', 1)[-1]
+            return '\t\t"label":"' + label + '",\n'
+
+        def getDescription():
+            if propertyShape.isSet['message']:
+                message = ''
+                for lang, message in propertyShape.message.items():
+                    message += '"' + lang + '":"' + message + '", '
+            elif propertyShape.isSet['description']:
+                message = '"en":"' + propertyShape.description + '"'
+            else:
+                message = '"en":"' + propertyShape.path.rsplit('/', 1)[-1] + '"'
+            return '\t\t"label":{' + message + '},\n'
+
+        def getCardinality():
+            cardinality = '\t\t"cardinality":{'
+
+            if propertyShape.isSet['minCount']:
+                cardinality += '"min": ' + propertyShape.minCount + ','
+            else:
+                cardinality += '"min": 0,'
+
+            if propertyShape.isSet['maxCount']:
+                cardinality += ' "max": ' + propertyShape.maxCount
+
+            cardinality += '}\n'
+            return cardinality
+
+        def getChoices():
+            choiceItem = ''
+            for index in range(0, len(propertyShape.shIn)):
+                choiceItem += '{"_reference: "' + propertyShape.shIn[index] + '"}'
+                choices = '{"value": "' + propertyShape.shIn[index] + '"},\n'
+                choices += '{"label": "' + propertyShape.shIn[index] + '"}\n}'
+
+                if index != len(propertyShape.shIn - 1):
+                    choiceItem += ',\n'
+                    choices += ','
+
+            choiceItem += ']\n},'
+            choiceItem += choices + '\n]'
+            return choiceItem
+
+        template = ['\t{\n']
 
         if isinstance(propertyShape.path, dict):
             # TODO handle complex paths (inverse, oneOrMorePath ...)
@@ -94,52 +155,31 @@ class RDFormsSerializer:
             # TODO handle sequence paths
             self.logger.info('Sequence path not supported, yet')
         else:
-            if propertyShape.isSet['shIn']:
-                # choice type
-                choiceItem = '"id":"' + propertyShape.path + '",\n"type":"choice",\n'
+            template.append(getId())
+            template.append(getType())
+            template.append(getLabel())
+            template.append(getDescription())
+            template.append(getCardinality())
+            template.append('\t},')
+            return ''.join(template)
 
-                self.logger.debug('Labels for choice items could be needed as well (message?)')
+            # choiceItem += 'choices: [{\n' + '"value: "' + propertyShape.path + '",\n'
+            # self.logger.debug(
+            #     'choices and single choice items have labels and descriptions again')
+            # choiceItem += '"children":[\n'
+            #
+            # for index in range(0, len(propertyShape.shIn)):
+            #     choiceItem += '{"_reference: "' + propertyShape.shIn[index] + '"}'
+            #     choices = '{"value": "' + propertyShape.shIn[index] + '"},\n'
+            #     choices += '{"label": "' + propertyShape.shIn[index] + '"}\n}'
+            #
+            #     if index != len(propertyShape.shIn - 1):
+            #         choiceItem += ',\n'
+            #         choices += ','
+            #
+            # choiceItem += ']\n},'
+            # choiceItem += choices + '\n]'
 
-                if propertyShape.isSet['message']:
-                    choiceItem += '"label":{'
-
-                    for lang, message in propertyShape.message.items():
-                        choiceItem += '"' + lang + '":"' + message + '", '
-
-                    choiceItem = choiceItem[0:-3]
-                    choiceItem += '},\n'
-
-                self.logger.debug('description exists as well again...')
-                choiceItem += '"property": "' + propertyShape.path + '",\n'
-
-                if propertyShape.isSet['minCount']:
-                    choiceItem += '"cardinality":{"min": ' + propertyShape.minCount + ','
-                else:
-                    choiceItem += '"cardinality":{"min": 1,'
-
-                if propertyShape.isSet['maxCount']:
-                    choiceItem += ' "max": ' + propertyShape.maxCount + '}\n'
-                else:
-                    choiceItem += ' "max": 1}\n'
-
-                choiceItem += 'choices: [{\n' + '"value: "' + propertyShape.path + '",\n'
-                self.logger.debug(
-                    'choices and single choice items have labels and descriptions again')
-                choiceItem += '"children":[\n'
-
-                for index in range(0, len(propertyShape.shIn)):
-                    choiceItem += '{"_reference: "' + propertyShape.shIn[index] + '"}'
-                    choices = '{"value": "' + propertyShape.shIn[index] + '"},\n'
-                    choices += '{"label": "' + propertyShape.shIn[index] + '"}\n}'
-
-                    if index != len(propertyShape.shIn - 1):
-                        choiceItem += ',\n'
-                        choices += ','
-
-                choiceItem += ']\n},'
-                choiceItem += choices + '\n]'
-
-        return choiceItem
 
 # x = RDFormsWriter()
 
